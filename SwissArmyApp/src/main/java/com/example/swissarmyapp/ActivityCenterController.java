e4bf996cf7dce26b2ad33b06f9e038f80de4fa46
Natelofgren <nate@lukesfamily.com>
2023-04-12 10:05:22 -0400

    Added buttons to Activity Center
    Yo!
    Added all the buttons to the Activity Center so you can go to all the scenes.
    Will have to make is look better in the future.

--- SwissArmyApp/src/main/java/com/example/swissarmyapp/ActivityCenterController.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/ActivityCenterController.java
@@ -19,4 +19,49 @@ public class ActivityCenterController {
         stage.setScene(scene);
         stage.show();
     }
+    @FXML
+    public void ToVerseOfTheDay(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("verse-of-the-day.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Verse Of The Day");
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void ToMadlibs(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("madlibs.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Madlibs");
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void ToSports(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sports.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Sports");
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void ToTrivia(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("trivia.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Trivia");
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void ToSettings(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("settings.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Settings");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
---
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/VerseOfTheDayController.java
@@ -0,0 +1,20 @@
+package com.example.swissarmyapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class VerseOfTheDayController {
+    public void Back(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("activity-center.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Activity Center");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
