0735fc8956299d4c491246a63be9fdf3062f3b01
Natelofgren <nate@lukesfamily.com>
2023-04-12 09:33:49 -0400

    Adding java files and scenes for future code
    Started by adding the landing page and the activity center

---
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/ChooseActivityController.java
@@ -0,0 +1,10 @@
+package com.example.swissarmyapp;
+
+import javafx.fxml.FXML;
+
+public class ChooseActivityController {
+    @FXML
+    protected void ToTipCalculator() {
+        System.out.println("ToTipCalculator");
+    }
+}
---
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/LandingPageController.java
@@ -0,0 +1,10 @@
+package com.example.swissarmyapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+public class LandingPageController {
+    @FXML
+    protected void ToChooseActvityPage() {
+        System.out.println("ToChooseActivityPage");
+    }
+}
