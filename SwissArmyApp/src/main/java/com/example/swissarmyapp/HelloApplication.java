c9363895bc8c3159b9058a140cb09288ba15f6cd
Natelofgren <nate@lukesfamily.com>
2023-04-12 11:55:06 -0400

    Continual Work On Settings
    Worked on settings a bit more

--- SwissArmyApp/src/main/java/com/example/swissarmyapp/HelloApplication.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/HelloApplication.java
@@ -8,6 +8,36 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public int fontSize = 12;
+
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+    }
+
+    public boolean isDarkMode() {
+        return darkMode;
+    }
+
+    public void setDarkMode(boolean darkMode) {
+        this.darkMode = darkMode;
+    }
+
+    public String getFont() {
+        return font;
+    }
+
+    public void setFont(String font) {
+        this.font = font;
+    }
+
+    private boolean darkMode =false;
+    private String font = "Normal";
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("landing-page.fxml"));
@@ -18,6 +48,8 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+
         launch();
     }
+
 }
