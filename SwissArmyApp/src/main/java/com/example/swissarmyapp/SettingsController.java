7f1f1b87fb2f6d5f2997c868362336aa56cbe717
Natelofgren <nate@lukesfamily.com>
2023-04-17 00:37:35 -0400

    Added a drop down
    Added a drop down in settings

--- SwissArmyApp/src/main/java/com/example/swissarmyapp/SettingsController.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/SettingsController.java
@@ -1,15 +1,27 @@
 package com.example.swissarmyapp;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SettingsController {
+    @FXML
+    private ChoiceBox fontChoice;
+    ObservableList<String> fontNames = FXCollections.observableArrayList("Times New Roman", "Ariel", "Comic Sans");
+    @FXML
+    private void initialize() {
+        fontChoice.setItems(fontNames);
+    }
+    @FXML
     public void Back(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("activity-center.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
