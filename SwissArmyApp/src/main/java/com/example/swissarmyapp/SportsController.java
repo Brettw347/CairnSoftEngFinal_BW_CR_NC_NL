9316b39064f2aae148598c9f1516b747283c2db0
NicoCione <112523384+NicoCione@users.noreply.github.com>
2023-04-28 15:36:03 -0400

    csv reader method now works
    The Eagles CSV stat sheet is now "readable" adn all data can be displayed on the sports page.

--- SwissArmyApp/src/main/java/com/example/swissarmyapp/SportsController.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/SportsController.java
@@ -5,11 +5,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class SportsController {
+
+public class SportsController extends EaglesCSV{
     public void Back(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("activity-center.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
@@ -19,4 +21,14 @@ public class SportsController {
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    private Label record;
+
+    @FXML
+    private void initialize() throws Exception {
+        com.example.swissarmyapp.EaglesCSV.generateStats();
+        record.setText("2023 Record (" + getWins() + "-" + getLoss() + "-" + getTies() + ")");
+    }
+
 }
