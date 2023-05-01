11fb9dda57c358e3bbb1b63159eab9ef8d022f48
NicoCione <112523384+NicoCione@users.noreply.github.com>
2023-05-01 00:26:01 -0400

    Unnecessary imports removed

--- SwissArmyApp/src/main/java/com/example/swissarmyapp/SportsController.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/SportsController.java
@@ -8,14 +8,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import java.io.IOException;
-import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.Scene;
-import javafx.scene.chart.PieChart;
-import javafx.scene.chart.PieChart.Data;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
 
 public class SportsController extends EaglesCSV{
